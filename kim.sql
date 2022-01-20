8d90afcfb2df1aa52a08707fb355b3e9ebed0570
kyulgon <83215027+nlee013@users.noreply.github.com>
2022-01-20 17:57:06 +0900

    Initial commit

---
+++ kim.sql
@@ -0,0 +1,4 @@
+select * from tab; --코드 옆에서 ctrl + ENTER 치면 data가 아래에 나옴
+
+select * from PERSONNEL;
+select * from DIVISION;
---
+++ lee.sql
@@ -0,0 +1,4 @@
+select * from tab; --코드 옆에서 ctrl + ENTER 치면 data가 아래에 나옴
+--한줄주석
+/* 여러주석*/
+select * from CUSTOM;
