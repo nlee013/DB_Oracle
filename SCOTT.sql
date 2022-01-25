f3cebde485a170283aec0a527caa89fdcd08de17
nlee013 <83215027+nlee013@users.noreply.github.com>
2022-01-25 18:39:43 +0900

    add

--- SCOTT.sql
+++ SCOTT.sql
@@ -17,7 +17,7 @@ SELECT ENAME, JOB, HIREDATE FROM EMP WHERE HIREDATE BETWEEN '81-02-20' AND '81-0
 SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE DEPTNO IN(20, 30) ORDER BY ENAME;
  
 --6. EMP 테이블에서 급여가 1500이상이고 부서번호가 10,30인 사원의 이름과 급여를 출력하는 SELECT 문장을 작성하여라. 단 HEADING을 Employee과 Monthly Salary로 출력하여라.
-SELECT ENAME, SAL FROM EMP WHERE SAL >= 1500 AND DEPTNO IN (10, 30);
+SELECT ENAME AS "Employee", SAL "Monthly Salary" FROM EMP WHERE SAL >= 1500 AND DEPTNO IN (10, 30);
  
 --7. EMP 테이블에서 1982년에 입사한 사원의 모든 정보를 출력하는 SELECT 문을 작성하여라.
 SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE HIREDATE >= '82-01-01';
@@ -35,8 +35,43 @@ SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE JOB IN('CLERK', 'ANALYST') 
 SELECT * FROM EMP WHERE REGEXP_COUNT (ENAME,'L') = 2 AND (MGR = 7782 OR DEPTNO = 30);
 SELECT * FROM EMP WHERE ENAME LIKE '%L%L%' AND (MGR = 7782 OR DEPTNO = 30);
 
+--------------2021.01.25.화-----------------------------
 
+--1. 현재 날짜를 출력하고 열 레이블은 Current Date로 출력하는 SELECT 문장을 기술하시오.
 
+SELECT CURRENT_DATE FROM DUAL;
 
+--2. EMP 테이블에서 현재 급여에 15%가 증가된 급여를 사원번호,이름,업무,급여,증가된 급여(New Salary),증가액(Increase)를 출력하는 SELECT 문장을 기술하시오.
+SELECT * FROM EMP;
+SELECT EMPNO, ENAME, JOB, SAL AS "New Salary", SAL*1.15 AS "Increase" from emp;
 
-;
+--3. EMP 테이블에서 이름,입사일,입사일로부터 6개월 후 돌아오는 월요일 구하여 출력하는 SELECT 문장을 기술하시오.
+select ename, hiredate, NEXT_DAY(ADD_MONTHS(hiredate,6),'월요일') from emp;
+
+--4. EMP 테이블에서 이름,입사일, 입사일로부터 현재까지의 월 수,급여, 입사일부터 현재까지의 급여의 총계를 출력하는 SELECT 문장을 기술하시오.
+SELECT ename, hiredate,ceil((SYSDATE - hiredate)/365*12), sal, floor(sal * (SYSDATE - hiredate)/365*12) from emp;
+
+select ename,hiredate,ceil(months_between(sysdate,hiredate)) as 월수,
+sal, floor(months_between(sysdate,hiredate)*sal) as 급여총계 from emp;
+--5. EMP 테이블에서 다음의 결과가 출력되도록 작성하시오.
+--Dream Salary
+------------------------------------------------------------
+--3배 차이
+--KING earns $5,000.00 monthly but wants $15,000.00
+--BLAKE earns $2,850.00 monthly but wants $8,550.00
+--CLARK earns $2,450.00 monthly but wants $7,350.00
+--. . . . . . . . . .
+--14 rows selected
+SELECT ENAME  ||' earns '|| TO_CHAR(sal, '$99,999.99') ||' monthly but wants '|| TO_CHAR(SAL*3, '$999,999.99')  as "Dream Salary" FROM EMP;
+
+--6. EMP 테이블에서 모든 사원의 이름과 급여(15자리로 출력 좌측의 빈곳은 “*”로 대치)를 출력하는 SELECT 문장을 기술하시오.
+SELECT ename, lpad(sal, 15, '*') from emp;
+
+--7. EMP 테이블에서 모든 사원의 정보를 이름,업무,입사일,입사한 요일을 출력하는 SELECT 문장을 기술하시오.
+SELECT ename, job, hiredate, TO_CHAR(hiredate, 'day') as 요일 from emp; 
+
+--8. EMP 테이블에서 이름의 길이가 6자 이상인 사원의 정보를 이름,이름의 글자수,업무를 출력하는 SELECT 문장을 기술하시오.
+select * from emp where LENGTH(ename) >= 6 ;
+
+--9. EMP 테이블에서 모든 사원의 정보를 이름,업무,급여,보너스,급여+보너스를 출력하는 SELECT 문장을 기술하시오.
+SELECT ename, job, sal, nvl(comm, 0) as bonus, (sal+nvl(comm, 0)) as "Pay+Bonus" from emp;
