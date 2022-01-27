fb215939a2d75096e0842913679225eadd7df569
nlee013 <83215027+nlee013@users.noreply.github.com>
2022-01-27 18:54:37 +0900

    ADD

--- SCOTT.sql
+++ SCOTT.sql
@@ -178,5 +178,80 @@ MANAGER	         2450         2975          2850          8275
 ANALYST	                      6000                        6000
 */
 
+----------2021.01.27.목------------
+
+--1. EMP 테이블에서 모든 사원에 대한 이름,부서번호,부서명을 출력하는 SELECT 문장을 작성하여라.
+
+SELECT * FROM EMP;
+
+SELECT  EMPNO, ENAME, DEPTNO FROM EMP;
+
+--2. EMP 테이블에서 NEW YORK에서 근무하고 있는 사원에 대하여 이름,업무,급여,부서명을 출력하는 SELECT 문장을 작성하여라.
+SELECT * FROM DEPT;
+--공통된 PK 찾기.DEPTNO
+SELECT ENAME, JOB, SAL, A.DEPTNO FROM EMP A, DEPT B WHERE A.DEPTNO = B. DEPTNO AND B.LOC = 'NEW YORK';
+
+--3. EMP 테이블에서 보너스를 받는 사원에 대하여 이름,부서명,위치를 출력하는 SELECT 문장을 작성하여라.
+
+SELECT * FROM EMP WHERE COMM IS NOT NULL;
+SELECT ENAME, A.DEPTNO, COMM, LOC FROM EMP A, DEPT B WHERE COMM > 0;
+
+
+--4. EMP 테이블에서 이름 중 L자가 있는 사원에 대하여 이름,업무,부서명,위치를 출력하는 SELECT 문장을 작성하여라.
+
+SELECT ENAME, A.DEPTNO, LOC FROM EMP A, DEPT B WHERE A.DEPTNO = B.DEPTNO AND A.ENAME LIKE '%L%';
+
+--5. 아래의 결과를 출력하는 SELECT 문장을 작성하여라.(관리자가 없는 King을 포함하여 모든 사원을 출력)-SELF JOIN
+SELECT * FROM EMP;
+SELECT A.ENAME "Employee", A.EMPNO "Emp#", B.ENAME "Manager", B.EMPNO "Mgr#" FROM EMP A, EMP B
+WHERE A.MGR = B.EMPNO(+) ORDER BY B.ENAME DESC, A.ENAME;
+--Employee        Emp# Manager         Mgr#
+--
+------------ --------- ---------- ---------
+--
+--KING            7839
+--
+--BLAKE           7698 KING            7839
+--
+--CLARK           7782 KING            7839
+--. . . . . . . . . .
+--14 rows selected.
+--SELECT A.*
+--FROM 
+--6. EMP 테이블에서 그들의 관리자 보다 먼저 입사한 사원에 대하여 이름,입사일,관리자 이름, 관리자 입사일을 출력하는 SELECT 문장을 작성하여라.
+SELECT * FROM EMP;
+
+SELECT A.ENAME SALESMAN, A.HIREDATE, B.ENAME MANAGER, B.HIREDATE FROM EMP A, EMP B
+WHERE A.MGR = B.EMPNO AND A.HIREDATE < B.HIREDATE;
+--TO_CHAR(HIREDATE,'YY-MM-DD') = 'SALESMAN', TO_CHAR(HIREDATE,'YY-MM-DD') = 'MANAGER';
+
+--7. EMP 테이블에서 사원의 급여와 사원의 급여 양만큼 “*”를 출력하는 SELECT 문장을 작성하여라. 단 “*”는 100을 의미한다.-LPAD
+--
+--Employee and their salary
+--
+-------------------------------------------------------------------
+--
+--KING      **************************************************
+--
+--BLAKE     ****************************
+--
+--CLARK     ************************
+--
+--JONES     *****************************
+--
+--MARTIN    ************
+--
+--ALLEN     ****************
+--
+--TURNER    ***************
+--
+--. . . . . . . . . .
+--
+--14 rows selected.
+
+SELECT ENAME||'   '|| LPAD(' ', SAL/100 + 1,'*') AS "Employee and their story" FROM EMP;
+SELECT ENAME||'   '|| LPAD('*', SAL/100,'*') AS "Employee and their story" FROM EMP;
+ 
+SELECT * FROM EMP;
 
  
